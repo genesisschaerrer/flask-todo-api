f2ba2f8078f5b42007587521527c442a90c62705
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-01-07 09:07:39 -0700

    added flask cors and heroku

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS 
+from flask_heroku import Heroku
 import os
 
 
 app=Flask(__name__)
+CORS(app)
+heroku = Heroku(app)
+DATABASE_URL = os.eviron.get("DATABASE_URL")
 
 #this lets python know exactly where this file lives.
 basedir = os.path.abspath(os.path.dirname(__file__))
 #We are creating a new key value pair in the config dictionary(object)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
 
 db = SQLAlchemy(app)
